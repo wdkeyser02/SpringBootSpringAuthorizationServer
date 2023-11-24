996253ef6913ce1533c58381e2f1acf88cd0ce65
willydekeyser <ma074172@skynet.be>
2023-11-24 09:17:30 +0100

    Setup OAuth2

---
+++ SpringBootClient/src/main/java/willydekeyser/SpringBootClientApplication.java
@@ -0,0 +1,13 @@
+package willydekeyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootClientApplication.class, args);
+	}
+
+}
